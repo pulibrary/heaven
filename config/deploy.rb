22ec01526b8f710735093f837d45b196bd285406
Trey Terrell <tterrell@princeton.edu>
2015-09-30 11:08:43 -0700

    Don't force SSL

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,7 @@ set :deploy_to, '/opt/appdeploy'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('db/production.sqlite3')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
@@ -33,6 +34,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+# Restart Passenger with touch.
+set :passenger_restart_with_touch, true
 
 namespace :deploy do
 
